22efa6421bcea7f5ff98db0be1743eb9fa62a11c
majdfathallah99 <majdfathallah3@gmail.com>
2025-08-13 08:35:47 +0200

    Add files via upload

---
+++ bonus_app_clean_v2/bonus_app_fix_access/hr_department_bonus_app_clean/__manifest__.py
@@ -0,0 +1,23 @@
+
+# -*- coding: utf-8 -*-
+{
+    "name": "HR Department Bonus (App)",
+    "version": "17.0.3.1.0",
+    "summary": "Standalone Bonuses app: requests & department budgets",
+    "category": "Human Resources",
+    "author": "You",
+    "license": "LGPL-3",
+    "images": ["static/description/icon.png"],
+    "depends": ["hr", "mail"],
+    "data": [
+        "data/hr_department_bonus_sequence.xml",
+        
+        "views/hr_bonus_type_views.xml",
+        "views/hr_bonus_request_views.xml",
+        "views/hr_department_views_inherit.xml",
+        "views/hr_department_bonus_menu.xml"
+    ],
+    "application": True,
+    "installable": True,,
+    "post_init_hook": "post_init_hook"
+}

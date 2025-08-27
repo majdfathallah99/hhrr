eaaba7cc4015a43bf0779029ee2388e8526dd276
majdfathallah99 <majdfathallah3@gmail.com>
2025-08-27 10:09:48 +0200

    Add files via upload

---
+++ hr_timeoff_late_block_v9/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Time Off: Block Late Submissions',
+    'summary': 'Block Time Off requests submitted more than 2 days after the leave start date (company setting).',
+    'version': '17.0.1.0.0',
+    'author': 'ChatGPT',
+    'website': 'https://example.com',
+    'license': 'LGPL-3',
+    'category': 'Human Resources/Time Off',
+    'depends': ['hr', 'hr_holidays'],
+    'data': ['views/res_company_view.xml', 
+        'views/res_config_settings_view.xml',
+            ],
+    'installable': True,
+    'application': False,
+}
---
+++ hr_timeoff_late_block_v9/models/res_company.py
@@ -0,0 +1,9 @@
+from odoo import models, fields
+
+class ResCompany(models.Model):
+    _inherit = "res.company"
+
+    restrict_late_timeoff_submission = fields.Boolean(
+        string="Block late Time Off submissions (> 2 days)",
+        help="When enabled, employees cannot submit a Time Off request more than 2 days after the leave start date.",
+    )

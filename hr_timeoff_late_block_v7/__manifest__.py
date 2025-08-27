7139aa7be3e79f3877a8f305b7a47bae16a74057
majdfathallah99 <majdfathallah3@gmail.com>
2025-08-27 09:52:27 +0200

    Add files via upload

---
+++ hr_timeoff_late_block_v7/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Time Off: Block Late Submissions',
+    'summary': 'Block Time Off requests submitted more than 2 days after the leave start date (company setting).',
+    'version': '17.0.1.0.0',
+    'author': 'ChatGPT',
+    'website': 'https://example.com',
+    'license': 'LGPL-3',
+    'category': 'Human Resources/Time Off',
+    'depends': ['hr', 'hr_holidays'],
+    'data': [
+        'views/res_config_settings_view.xml',
+        'views/cleanup_disable_company_view.xml',
+    ],
+    'installable': True,
+    'application': False,
+}

d610f5df549ab41a0dbb9e95435250ffc824ffdb
majdfathallah99 <majdfathallah3@gmail.com>
2025-08-27 11:06:55 +0200

    Delete hr_timeoff_late_block_v11 directory

--- hr_timeoff_late_block_v11/models/res_company.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields
-
-class ResCompany(models.Model):
-    _inherit = "res.company"
-
-    restrict_late_timeoff_submission = fields.Boolean(
-        string="Block late Time Off submissions (> 2 days)",
-        help="When enabled, employees cannot submit a Time Off request more than 2 days after the leave start date.",
-    )

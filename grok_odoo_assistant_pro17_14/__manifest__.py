885358a14406d89b8cb7ec12b7b2a895e84866ac
majdfathallah99 <majdfathallah3@gmail.com>
2025-09-01 23:32:31 +0200

    Add files via upload

---
+++ grok_odoo_assistant_pro17_14/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Grok Odoo Assistant Pro (AR/EN, Safer Tools)",
+    "version": "17.0.1.2.3",
+    "summary": "Bilingual conversational assistant with safe tool execution and voice I/O",
+    "depends": ["base", "web"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/menus.xml",
+        "views/ui_templates.xml",
+        "views/config_templates.xml",
+        "views/session_views.xml"
+    ],
+    "application": True,
+    "installable": True,
+    "license": "LGPL-3"
+}
